57ef4b5950eae5936f2727b529dc7abfb5df938e
Dolganov_830501 <dolganovvlad486@gmail.com>
2023-11-03 00:02:31 +0300

    create heroes

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+export const heroDelete = (id) => {
+    return {
+        type: 'HEROES_DELETED',
+        payload: id
+    }
+}
---
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,91 @@
+import {useHttp} from '../../hooks/http.hook';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+// Уникальный идентификатор персонажа можно сгенерировать через uiid
+// Усложненная задача:
+// Персонаж создается и в файле json при помощи метода POST
+// Дополнительно:
+// Элементы <option></option> желательно сформировать на базе
+// данных из фильтров
+
+const HeroesAddForm = () => {
+     // Состояния для контроля формы
+     const [heroName, setHeroName] = useState('');
+     const [heroDescr, setHeroDescr] = useState('');
+     const [heroElement, setHeroElement] = useState('');
+
+
+    const onSubmitHandler = (e)=>{
+        e.preventDefault();
+        
+        const newHero ={
+            id: uuidv4(),
+            name: heroName,
+            description: heroDescr,
+            element: heroElement
+        }
+
+
+        setHeroName('');
+        setHeroDescr('');
+        setHeroElement('');
+
+    }
+
+
+    return (
+        <form className="border p-4 shadow-lg rounded" onSubmit={onSubmitHandler}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    value={heroName}
+                    onChange={(e)=>{setHeroName(e.target.value)}}
+                    placeholder="Как меня зовут?"/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    value={heroDescr}
+                    onChange={(e)=>{setHeroDescr(e.target.value)}}
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}/>
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    value={heroElement}
+                    onChange={(e)=>{setHeroElement(e.target.value)}}
+                    name="element">
+                    <option >Я владею элементом...</option>
+                    <option value="fire">Огонь</option>
+                    <option value="water">Вода</option>
+                    <option value="wind">Ветер</option>
+                    <option value="earth">Земля</option>
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary">Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
